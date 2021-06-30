65c2f4274afa9a54e82708d04c341f498bdea3d9
Jay Jenks <jamesljenks@gmail.com>
2021-06-29 21:45:48 -0400

    Updated code for category-routes.js file.

--- models/index.js
+++ models/index.js
@@ -16,20 +16,14 @@ Category.hasMany(Product, {
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: {
-    model: ProductTag,
-    unique: false
-  },
+  through: ProductTag,
   as: 'tags',
   foreignKey: 'product_id'
 })
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: {
-    model: ProductTag,
-    unique: false
-  },
+  through: ProductTag,
   as: 'products',
   foreignKey: 'tag_id'
 })
